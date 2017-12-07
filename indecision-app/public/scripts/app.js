e99785b94b3e5962d8e560ce148c8e1a256c1b0e
erickstm87 <erickstm87@github.com>
2017-12-06 22:10:07 -0700

    just started actual jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,12 +3,37 @@
 console.log('hello again');
 
 //JSX - Javascript XML
-var template = React.createElement(
-  'h1',
+// var template = (
+// <div>
+//     <h1>more jsx stuff right?</h1>
+//     <p>This is some info</p>
+//     <ol>
+//         <li>list one</li>
+//         <li>list two</li>
+//     </ol>
+// </div>
+// );
+
+var templateTwo = React.createElement(
+  'div',
   null,
-  'more jsx stuff right?'
+  React.createElement(
+    'h1',
+    null,
+    'Tomas Erickson'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 30'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Boulder'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,25 @@
 console.log('hello again');
 
 //JSX - Javascript XML
-var template = <h1>more jsx stuff right?</h1>;
+// var template = (
+// <div>
+//     <h1>more jsx stuff right?</h1>
+//     <p>This is some info</p>
+//     <ol>
+//         <li>list one</li>
+//         <li>list two</li>
+//     </ol>
+// </div>
+// );
+
+var templateTwo = (
+    <div>
+      <h1>Tomas Erickson</h1>
+      <p>Age: 30</p>
+      <p>Location: Boulder</p>  
+    </div>
+);
 
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
