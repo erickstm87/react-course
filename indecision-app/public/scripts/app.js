ede9cfdf719a475b424f7d17a6ad80c88f69a573
erickstm87 <erickstm87@github.com>
2017-12-06 20:20:59 -0700

    added babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,12 +1,14 @@
+'use strict';
+
 console.log('hello again');
 
 //JSX - Javascript XML
-//var template = <p>more jsx stuff</p>;
 var template = React.createElement(
-    'h1',
-    { id: 'someid' },
-    'this is jsx stuff'
-  );
-var appRoot = document.getElementById('app')
+  'h1',
+  null,
+  'more jsx stuff right?'
+);
+
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,8 @@
+console.log('hello again');
+
+//JSX - Javascript XML
+var template = <h1>more jsx stuff right?</h1>;
+
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot);
