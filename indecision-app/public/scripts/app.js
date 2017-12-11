bfbe82b314eb362b64e4ab9b912f2b71d747b4fd
erickstm87 <erickstm87@github.com>
2017-12-11 07:08:31 -0700

    made a button that conditionally renders paragraph

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,92 +1,36 @@
 'use strict';
 
-console.log('hello again');
-
-var app = {
-    title: 'Indecision App',
-    subtitle: 'What Should I do Hal?',
-    options: []
-};
 var appRoot = document.getElementById('app');
 
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * app.options.length);
-    console.log(randomNum);
-    var option = app.options[randomNum];
-    console.log(option);
-};
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
-    var option = e.target.elements.option.value;
-
-    if (option) {
-        //if string is empty that's falsey so this if statement won't run
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        console.log(app.options);
-    }
-    renderTemplateApp();
+var declared = void 0;
+var flipSwitch = function flipSwitch() {
+    declared = !declared;
+    console.log('button was clicked');
+    renderTemplate();
 };
 
-var emptyArray = function emptyArray() {
-    app.options = [];
-    renderTemplateApp();
-};
-
-var renderTemplateApp = function renderTemplateApp() {
-    //JSX - Javascript XML
+var renderTemplate = function renderTemplate() {
     var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            app.title
-        ),
-        app.subtitle && React.createElement(
-            'p',
-            null,
-            app.subtitle
+            'Visibility Toggle'
         ),
         React.createElement(
             'button',
-            { disabled: app.options.length === 0, onClick: onMakeDecision },
-            'What should I do?'
-        ),
-        React.createElement(
-            'button',
-            { onClick: emptyArray },
-            'Remove All Entries'
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add Option'
-            )
+            { onClick: flipSwitch },
+            declared ? 'Hide Details' : 'Show Details'
         ),
         React.createElement(
             'p',
             null,
-            app.options.length > 0 ? 'Here are your options' : 'No options'
-        ),
-        React.createElement(
-            'ol',
-            null,
-            app.options.map(function (option) {
-                return React.createElement(
-                    'li',
-                    { key: option + 1 },
-                    option
-                );
-            })
+            declared ? 'here are the deets' : ''
         )
     );
 
     ReactDOM.render(template, appRoot);
 };
 
-renderTemplateApp();
+renderTemplate();
---
+++ indecision-app/src/playground/build-it-visible.js
@@ -0,0 +1,22 @@
+const appRoot = document.getElementById('app');
+
+let declared;
+const flipSwitch = () => {
+    declared = !declared;
+    console.log('button was clicked');
+    renderTemplate();
+}
+
+const renderTemplate = () => {
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={flipSwitch}>{declared ? 'Hide Details' : 'Show Details'}</button>
+            <p>{declared ? 'here are the deets' : ''}</p>
+        </div>
+    )
+
+    ReactDOM.render(template, appRoot);
+}
+
+renderTemplate();
