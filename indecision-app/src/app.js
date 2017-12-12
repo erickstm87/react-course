e8adbe3ac7542d536d3c74d894f73c9984561776
erickstm87 <erickstm87@github.com>
2017-12-11 21:56:11 -0700

    got started on props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,59 +1,68 @@
-console.log('hello again');
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'What Should I do Hal?',
-    options: []
-}
-const appRoot = document.getElementById('app');
-
-const onMakeDecision = () => {
-    const randomNum = Math.floor((Math.random() * app.options.length));
-    console.log(randomNum);
-    const option = app.options[randomNum];
-    console.log(option);
-}
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-
-    if(option){ //if string is empty that's falsey so this if statement won't run
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        console.log(app.options);
+class IndecisionApp extends React.Component {
+    render(){
+        return(
+            <div>
+                <Header title='The Indecision' />
+                <Action />
+                <Options />
+                <AddOptions />
+            </div>
+        )
     }
-    renderTemplateApp();
-};
-
-const emptyArray = () => {
-    app.options = [];
-    renderTemplateApp();
 }
 
-const renderTemplateApp = () => 
-{
-    //JSX - Javascript XML
-    const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-        <button onClick={emptyArray}>Remove All Entries</button>
-        <form onSubmit={onFormSubmit}>
-            <input type='text' name='option'/>
-            <button>Add Option</button>
-        </form>
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-        {
-                app.options.map((option) => <li key={option + 1}>{option}</li> )
-        }
-        </ol>
-        
-    </div>
-    );
 
-    ReactDOM.render(template, appRoot);
+class Action extends React.Component {
+    render(){
+        return (
+            <div>
+            <button>What should i do?</button>
+            </div>
+        )
+    }
 }
 
-renderTemplateApp();
+class Options extends React.Component {
+    render(){
+        return(
+            <div>
+                <Option /> 
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render(){
+        return(
+            <div>
+                <p>a</p>
+                <p>b</p>
+                <p>c</p>
+            </div>
+        )
+    }
+}
+
+class AddOptions extends React.Component {
+    render(){
+        return(
+            <div>
+                <p>Here is my addoptions component</p>    
+            </div>        
+        )
+    }
+}
+
+class Header extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>Put your life into the hands of the machine</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
