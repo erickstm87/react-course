7a6c6826b2c43247bfb4b49d33d447056515887d
erickstm87 <erickstm87@github.com>
2017-12-31 10:45:01 -0700

    started on routing

--- expense-reports/src/app.js
+++ expense-reports/src/app.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>this is content</p>, document.getElementById('app')); 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard router
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my ad expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is to edit my expenses
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is my help page
+    </div>
+);
+
+const routes = (
+    <BrowserRouter> 
+        <div>
+            <Route exact path='/' component={ExpenseDashboardPage}/>
+            <Route path='/create' component={AddExpensePage}/>
+            <Route path='/edit' component={EditExpensePage}/>
+            <Route path='/help' component={HelpPage}/>
+        </div>   
+     </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app')); 
 //ReactDOM.render(<User name='tomas' age={30}/>, document.getElementById('app')); 
