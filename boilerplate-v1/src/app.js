660675973dfa48cb8748a47556fc70e57606eb1f
erickstm87 <erickstm87@github.com>
2017-12-31 10:09:55 -0700

    added a starting point for expensify as well as a boiler plate option

---
+++ boilerplate-v1/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>this is content</p>, document.getElementById('app')); 
+//ReactDOM.render(<User name='tomas' age={30}/>, document.getElementById('app')); 
