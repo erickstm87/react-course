12993ecca31900a80e9e567c32722beaf33125cc
erickstm87 <erickstm87@github.com>
2017-12-31 22:34:02 -0700

    internal routing

--- expense-reports/src/app.js
+++ expense-reports/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, BrowserRouter, Route} from 'react-router-dom';
+import { Switch, BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,19 +30,32 @@ const HelpPage = () => (
 
 const NotFound = () => (
     <div>
-        404 NOT FOUND!
+        404 NOT FOUND! - <Link to='/'>Home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to='/' activeClassName='is-active' exact> [Dashboard] </NavLink>
+        <NavLink to='/create' activeClassName='is-active'> [Create Stuff] </NavLink>
+        <NavLink to='/edit' activeClassName='is-active'> [Edit Stuff that already exists] </NavLink>
+        <NavLink to='/help' activeClassName='is-active'> [In Need of Assistance] </NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter> 
-        <Switch>
-            <Route exact path='/' component={ExpenseDashboardPage}/>
-            <Route path='/create' component={AddExpensePage}/>
-            <Route path='/edit' component={EditExpensePage}/>
-            <Route path='/help' component={HelpPage}/>
-            <Route component={NotFound}/>
-        </Switch>   
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={ExpenseDashboardPage}/>
+                <Route path='/create' component={AddExpensePage}/>
+                <Route path='/edit' component={EditExpensePage}/>
+                <Route path='/help' component={HelpPage}/>
+                <Route component={NotFound}/>
+            </Switch>  
+        </div> 
      </BrowserRouter>
 );
 
