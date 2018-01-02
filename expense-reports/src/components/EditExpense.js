582ebf5e65502f8112e5bb9b6a0c92226b2b6707
erickstm87 <erickstm87@github.com>
2018-01-02 11:08:05 -0700

    query string params for reactrouter

--- expense-reports/src/components/EditExpense.js
+++ expense-reports/src/components/EditExpense.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 
-const EditExpensePage = () => (
-    <div>
-        This is to edit my expenses
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            This is to edit the expense {props.match.params.id}
+        </div>
+    )
+};
 
 export default EditExpensePage;
--- expense-reports/src/components/Header.js
+++ expense-reports/src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to='/' activeClassName='is-active' exact> [Dashboard] </NavLink>
         <NavLink to='/create' activeClassName='is-active'> [Create Stuff] </NavLink>
-        <NavLink to='/edit' activeClassName='is-active'> [Edit Stuff that already exists] </NavLink>
         <NavLink to='/help' activeClassName='is-active'> [In Need of Assistance] </NavLink>
     </header>
 );
