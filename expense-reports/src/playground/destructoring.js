cbadb9a514e28b3595f6ce1a96640b8b9fec0b6f
erickstm87 <erickstm87@github.com>
2018-01-07 16:47:49 -0700

    destructuring for arrays

--- expense-reports/src/playground/destructoring.js
+++ expense-reports/src/playground/destructoring.js
@@ -15,14 +15,27 @@
 
 // console.log(`${city} is ${temp} degrees right now`);
 
+const location = ['123 brook st.', 'brooklyn', 'ny', '40012'];
+
+const [, , state = 'Pennsylvania', zipCode] = location; // <-- This is used as an ordered list so address doesn't actually match anything it just uses the first element[0] in the array because it was the first variable to get assigned. In this example I just used the last two variables
+
+console.log(`The address is here: ${state} and the zip code is here: ${zipCode}`);
+
 const book = {
     title: 'Deep Work',
     author: 'cal newport',
     publisher: {
-       //name: 'Penguin'
+       name: 'Penguin',
+       anotherAddress: 'a street'
     }
 };
 
-const {name: publisherName = 'couldn\'t tell ya'} = book.publisher;
+const {name: publisherName = 'couldn\'t tell ya', anotherAddress} = book.publisher;
 
-console.log(publisherName);
+console.log(publisherName, anotherAddress);
+
+const item = ['coffee', '3', '2', '1'];
+
+const [drink, , mediumPrice,] = item;
+
+console.log(`This item of ${drink} is medium sized so costs this much ${mediumPrice}`);
