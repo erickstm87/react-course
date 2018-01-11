b112e95e1e2bb82c20904b5517aadfb3bc179d01
Thomas Erickson <terickson@spotx.tv>
2018-01-10 17:18:32 -0700

    redux complications

--- expense-reports/src/playground/redux-expensify.js
+++ expense-reports/src/playground/redux-expensify.js
@@ -125,6 +125,13 @@ const getVisibleExpenses = (expense, { text, sortBy, startDate, endDate}) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a,b) => {
+        if(sortBy === 'date'){
+            return a.createdAt < b.createdAt ? 1 : -1;            
+        }
+        else if(sortBy === 'amount'){
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
@@ -141,15 +148,15 @@ store.subscribe(() => {
     console.log(visibleExpenses);    
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 5000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -5000 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}));
 
-store.dispatch(setTextFilter('t'));
+//store.dispatch(setTextFilter('t'));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+ store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 // store.dispatch(setStartDate(125));
 // store.dispatch(setEndDate(32));
