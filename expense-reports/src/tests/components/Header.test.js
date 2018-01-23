39384c5daf4c3bc1259607c33286f040dbd90cf1
Thomas Erickson <terickson@spotx.tv>
2018-01-23 08:09:08 -0700

    enzyme introduction

--- expense-reports/src/tests/components/Header.test.js
+++ expense-reports/src/tests/components/Header.test.js
@@ -1,10 +1,14 @@
 //react-test-renderer
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
 import Header from '../../components/Header';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    //expect(wrapper.find('h1').text()).toBe('Expensify');
+    const wrapper = shallow(<Header />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
 });
