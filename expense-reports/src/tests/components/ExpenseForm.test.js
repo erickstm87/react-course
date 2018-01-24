1b58888eb0216e5213fcbe6951226b7de08b259c
Thomas Erickson <terickson@spotx.tv>
2018-01-24 07:46:16 -0700

    submitted user interaction

--- expense-reports/src/tests/components/ExpenseForm.test.js
+++ expense-reports/src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,48 @@ test('should render expense form with expense form data', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[1]}/>);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render an error when user submits wrong input', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+    const value = 'new description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('description')).toBe(value);
+});
+
+test('should set a note in text area', () => {
+    const value = 'a new note';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value);
+});
+
+test('should set amount if valid input', () => {
+    const value = '23.56';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe(value);
+});
+
+test('should not set amount if invalid input', () => {
+    const value = '12.12.12';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: {value}
+    });
+    expect(wrapper.state('amount')).toBe('');
+});
