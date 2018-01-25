607cc6404915180256c980777a6cea45f050773b
Thomas Erickson <terickson@spotx.tv>
2018-01-25 12:59:26 -0700

    commit for question

--- expense-reports/src/tests/components/ExpenseListFilters.test.js
+++ expense-reports/src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
+import moment from 'moment';
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -32,4 +33,45 @@ test('should render ExpenseListFilters correctly', () => {
         filters: altFilters
     });
     expect(wrapper).toMatchSnapshot();
+});
+
+//should handle text change
+test('should handle text changes', () => {
+    const value = 'Rent';
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+test('should sort by date', () => {
+    const value = 'date';
+    wrapper.setProps({
+        filters: altFilters
+    });
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByDate).toHaveBeenCalled();
+});
+test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByAmount).toHaveBeenCalled();
+});
+test('should handle date changes', () => {
+    const startDate = moment(0).add(4, 'years');
+    const endDate = moment(0).add(8, 'years');
+    wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate, endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);    
+});
+test('should handle date focus changes', () => {
+    const calendarFocused = 'endDate';
+    wrapper.find('DateRangePicker') ? console.log('found it') : console.log('couldn\'t find it');
+    wrapper.find('DateRangePicker').prop('onFocusChange') ? console.log('found it again') : console.log('couldn\'t find it')
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
